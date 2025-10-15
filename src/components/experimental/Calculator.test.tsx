117157b8da6f31563b6dbbfcdb26be3ff1808f2b
Jose Luis <wildchamo@gmail.com>
2025-10-15 17:41:35 -0500

    refactor: simplify calculator test cases and improve test description readability

--- src/components/experimental/Calculator.test.tsx
+++ src/components/experimental/Calculator.test.tsx
@@ -3,38 +3,25 @@ import { render, screen } from "@testing-library/react";
 import { Calculator } from "./Calculator";
 
 describe("<Calculator />", () => {
-  const useCasesTest = [
-    {
-      a: 1,
-      b: 2,
-      operation: "add" as const,
-      expected: 3,
-    },
-    {
-      a: 1,
-      b: 2,
-      operation: "subtract" as const,
-      expected: -1,
-    },
-    {
-      a: 1,
-      b: 2,
-      operation: "multiply" as const,
-      expected: 2,
-    },
-    {
-      a: 1,
-      b: 2,
-      operation: "divide" as const,
-      expected: 0.5,
-    },
+  // arrange
+
+  const testCases = [
+    { a: 1, b: 2, expected: 3, operation: "add" as const },
+    { a: 1, b: 2, expected: -1, operation: "subtract" as const },
+    { a: 1, b: 2, expected: 2, operation: "multiply" as const },
+    { a: 1, b: 2, expected: 0.5, operation: "divide" as const },
   ];
 
-  it.each(useCasesTest)(
-    "Debería calcular la operación $expected cuando $a y $b son $operation",
-    ({ a, b, operation, expected }) => {
+  // act
+
+  it.each(testCases)(
+    "debería retornar $expected para $operation con $a y $b",
+    ({ a, b, expected, operation }) => {
       render(<Calculator a={a} b={b} operation={operation} />);
-      const result = screen.getByText(`Result: ${expected}`);
+      const result = screen.getByText("Result: " + expected);
+
+      // assert
+
       expect(result).toBeInTheDocument();
     }
   );
