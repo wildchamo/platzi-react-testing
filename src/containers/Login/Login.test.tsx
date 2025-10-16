c8f9d46d7c66ec554ad05f4d2d8a31d5131bb50f
Jose Luis <wildchamo@gmail.com>
2025-10-15 21:00:55 -0500

    test: add login form error handling and input validation tests

--- src/containers/Login/Login.test.tsx
+++ src/containers/Login/Login.test.tsx
@@ -4,6 +4,7 @@ import { Login } from "./Login";
 import { MemoryRouter } from "react-router-dom";
 import { SessionProvider } from "../../context/AuthContext";
 import { getAuth } from "../../services/getAuth";
+import userEvent from "@testing-library/user-event";
 
 vi.mock("../../services/getAuth", () => {
   return {
@@ -14,7 +15,26 @@ vi.mock("../../services/getAuth", () => {
 const mockGetAuth = getAuth as Mock;
 
 describe("<Login />", () => {
-  it("deberia mostrar un mensaje de error", () => {
+  it("Inputs should be rendered", () => {
+    render(
+      <SessionProvider>
+        <MemoryRouter>
+          <Login />
+        </MemoryRouter>
+      </SessionProvider>
+    );
+
+    const usernameInput = screen.getByTestId("username");
+    const passwordInput = screen.getByTestId("password");
+    const buttonLogin = screen.getByRole("button", { name: "Login" });
+
+    expect(usernameInput).toBeInTheDocument();
+    expect(passwordInput).toBeInTheDocument();
+    expect(buttonLogin).toBeInTheDocument();
+  });
+
+  it("Error state", async () => {
+    const user = userEvent.setup();
     mockGetAuth.mockRejectedValueOnce(new Error("Invalid credentials"));
     render(
       <SessionProvider>
@@ -27,10 +47,12 @@ describe("<Login />", () => {
     const usernameInput = screen.getByTestId("username");
     const passwordInput = screen.getByTestId("password");
 
-    // const error = screen.getByText("Error");
-    // expect(error).toBeInTheDocument();
+    const buttonLogin = screen.getByRole("button", { name: "Login" });
 
-    expect(usernameInput).toBeInTheDocument();
-    expect(passwordInput).toBeInTheDocument();
+    await user.type(usernameInput, "test");
+    await user.type(passwordInput, "test");
+    await user.click(buttonLogin);
+    const error = screen.getByText("Invalid credentials");
+    expect(error).toBeInTheDocument();
   });
 });
